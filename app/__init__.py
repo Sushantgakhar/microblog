c85025af282fc3acb82d024622b83d28da8615f1
Sushant Gakhar <gakharsushant@gmail.com>
2016-10-16 20:23:16 +0530

    User avatars

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-# Make sure this happens after declaring app to avoid circular import error
+db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 
-from app import views
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
+
--- app/views.py
+++ app/views.py
@@ -1,30 +1,112 @@
-from flask import render_template, redirect, flash
-from app import app
-from .forms import LoginForm
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
+from .forms import LoginForm, EditForm
+from .models import User
+from datetime import datetime
 
-@app.route('/')
-@app.route('/index/')
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-def index():
-    user = {'nickname' : 'Sushant'}
-    posts = [
-	{
-		'author' : {'nickname' : 'Susan'},
-		'body' : 'Beautiful day in Portland!'
-	},
-	{
-		'author' : {'nickname' : 'John'},
-		'body' : 'The avengers movie was so cool'
-	}
- 	]
-    return render_template('index.html', user=user, title = 'Home', posts = posts, providers=app.config['OPENID_PROVIDERS'])
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.now()
+	db.session.add(g.user)
+	db.session.commit()
     
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = g.user
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %(form.openid.data, str(form.remember_me.data)))
-        return redirect('/index') 
-    return render_template('login.html', form=form, title='Sign In', providers=app.config['OPENID_PROVIDERS'])
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
 
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found' % nickname)
+        return redirect(url_for('index'))
+    posts=[ {'author': user, 'body' : 'Test Post #1'}, {'author': user, 'body':'Test post #2'} ]
+    return render_template('user.html', posts = posts, user = user)
+
+
+
+@app.route('/edit')
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+	g.user.about_me = form.about_me.data
+	db.session.add(g.user)
+	db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+	form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form = form)        
+	
+      
